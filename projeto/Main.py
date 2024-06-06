fd0cff82160b91a7629f8c775c8db05d136d52d6
Gabriel Lopes <carnagesagas@gmail.com>
2024-06-06 09:47:36 -0300

    puxar o relatório da anticamara no sistema

--- projeto/Main.py
+++ projeto/Main.py
@@ -2,9 +2,36 @@ import pyautogui
 import subprocess
 import time
 import os
+from datetime import datetime, timedelta
 
+# facilidar codar o caminho para os arquivos
 def caminho_imagem(imagem_nome):
     return os.path.join(os.path.dirname(__file__), 'img', imagem_nome)
+
+# retorna a data inicial do relatório
+def data_inicial():
+    hoje = datetime.now()
+    if hoje.weekday() == 0:
+        data_inicial = hoje - timedelta(days=2)
+    else:
+        data_inicial = hoje - timedelta(days=1)
+    return data_inicial.strftime('%d%m%Y')
+
+# retorna a data final de um dia atrás
+def data_final():
+    ontem = datetime.now() - timedelta(days=1)
+    return ontem.strftime('%d%m%Y')
+
+# função que insere as datas
+def inserir_data():
+    data_inicio = data_inicial()
+    pyautogui.write(data_inicio)
+    pyautogui.press('tab')
+    time.sleep(1)
+
+    data_fim = data_final()
+    pyautogui.write(data_fim)
+    time.sleep(1)
 
 # Abrir o programa
 caminho_app = r'C:\Program Files (x86)\Full Gauge\SitradRemote\SitradRemote.exe'
@@ -38,3 +65,34 @@ imagem_localizada = pyautogui.locateCenterOnScreen(imagem_gerador)
 x, y = imagem_localizada
 pyautogui.click(x, y)
 time.sleep(10)
+
+# puxar relátorio da anticamara
+imagem_anticamara = caminho_imagem('anti_camara.png')
+imagem_localizada = pyautogui.locateCenterOnScreen(imagem_anticamara)
+x, y = imagem_localizada
+pyautogui.doubleClick(x, y)
+time.sleep(2)
+inserir_data()
+imagem_temperaturaAmbiente = caminho_imagem('temperatura_ambiente.png')
+imagem_localizada = pyautogui.locateCenterOnScreen(imagem_temperaturaAmbiente)
+x, y = imagem_localizada
+pyautogui.click(x, y)
+imagem_dispositivo = caminho_imagem('dispositivo.png')
+imagem_localizada = pyautogui.locateCenterOnScreen(imagem_dispositivo)
+x, y = imagem_localizada
+pyautogui.click(x, y)
+imagem_adicionar = caminho_imagem('adicionar.png')
+imagem_localizada = pyautogui.locateCenterOnScreen(imagem_adicionar)
+x, y = imagem_localizada
+pyautogui.click(x, y)
+imagem_gerar = caminho_imagem('gerar.png')
+imagem_localizada = pyautogui.locateCenterOnScreen(imagem_gerar)
+x, y = imagem_localizada
+pyautogui.click(x, y)
+
+
+
+
+
+
+
