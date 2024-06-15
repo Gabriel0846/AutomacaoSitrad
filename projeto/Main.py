43189d58fb5536fa95be134f4f027361e8a89838
Gabriel Lopes <carnagesagas@gmail.com>
2024-06-15 10:54:52 -0300

    ajustes no codigo, função clicarimagem

--- projeto/Main.py
+++ projeto/Main.py
@@ -4,95 +4,73 @@ import time
 import os
 from datetime import datetime, timedelta
 
-# facilidar codar o caminho para os arquivos
+# Facilita o caminho para os arquivos
 def caminho_imagem(imagem_nome):
     return os.path.join(os.path.dirname(__file__), 'img', imagem_nome)
 
-# retorna a data inicial do relatório
+# Retorna a data inicial do relatório
 def data_inicial():
     hoje = datetime.now()
-    if hoje.weekday() == 0:
-        data_inicial = hoje - timedelta(days=2)
-    else:
-        data_inicial = hoje - timedelta(days=1)
+    data_inicial = hoje - timedelta(days=2) if hoje.weekday() == 0 else hoje - timedelta(days=1)
     return data_inicial.strftime('%d%m%Y')
 
-# retorna a data final de um dia atrás
+# Retorna a data final de um dia atrás
 def data_final():
     ontem = datetime.now() - timedelta(days=1)
     return ontem.strftime('%d%m%Y')
 
-# função que insere as datas
+# Função que insere as datas
 def inserir_data():
-    data_inicio = data_inicial()
-    pyautogui.write(data_inicio)
+    pyautogui.write(data_inicial())
     pyautogui.press('tab')
     time.sleep(1)
-
-    data_fim = data_final()
-    pyautogui.write(data_fim)
+    pyautogui.write(data_final())
     time.sleep(1)
+
+# Função para clicar em uma imagem
+def clicar_imagem(imagem, duplo_clique=False):
+    localizacao = pyautogui.locateCenterOnScreen(caminho_imagem(imagem))
+    if localizacao:
+        x, y = localizacao
+        if duplo_clique:
+            pyautogui.doubleClick(x, y)
+        else:
+            pyautogui.click(x, y)
+        time.sleep(1)
 
 # Abrir o programa
 caminho_app = r'C:\Program Files (x86)\Full Gauge\SitradRemote\SitradRemote.exe'
 subprocess.Popen(caminho_app)
 time.sleep(10)
 
-# digitar o login e senha
-imagem_servidor = caminho_imagem('servidor.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_servidor)
-x, y = imagem_localizada
-pyautogui.doubleClick(x, y)
-time.sleep(1)
+# Digitar o login e senha
+clicar_imagem('servidor.png', duplo_clique=True)
 pyautogui.write('admin123')
 pyautogui.press('enter')
 time.sleep(8)
 
-# clica em uma parte do programa para carregar os termometros
-imagem_carregar = caminho_imagem('carregar.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_carregar)
-x, y = imagem_localizada
-pyautogui.click(x, y)
-time.sleep(1)
+# Clica em uma parte do programa para carregar os termômetros
+clicar_imagem('carregar.png')
 
-# entrar na tela que puxa os relatórios
-imagem_arquivo = caminho_imagem('arquivo.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_arquivo)
-x, y = imagem_localizada
-pyautogui.click(x,y)
-imagem_gerador = caminho_imagem('gerador_de_relatorios.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_gerador)
-x, y = imagem_localizada
-pyautogui.click(x, y)
+# Entrar na tela que puxa os relatórios
+clicar_imagem('arquivo.png')
+clicar_imagem('gerador_de_relatorios.png')
 time.sleep(10)
 
-# puxar relátorio da anticamara
-imagem_anticamara = caminho_imagem('anti_camara.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_anticamara)
-x, y = imagem_localizada
-pyautogui.doubleClick(x, y)
+# Puxar relatório da anticâmara
+clicar_imagem('anti_camara.png', duplo_clique=True)
 time.sleep(2)
 inserir_data()
-imagem_temperaturaAmbiente = caminho_imagem('temperatura_ambiente.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_temperaturaAmbiente)
-x, y = imagem_localizada
-pyautogui.click(x, y)
-imagem_dispositivo = caminho_imagem('dispositivo.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_dispositivo)
-x, y = imagem_localizada
-pyautogui.click(x, y)
-imagem_adicionar = caminho_imagem('adicionar.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_adicionar)
-x, y = imagem_localizada
-pyautogui.click(x, y)
-imagem_gerar = caminho_imagem('gerar.png')
-imagem_localizada = pyautogui.locateCenterOnScreen(imagem_gerar)
-x, y = imagem_localizada
-pyautogui.click(x, y)
+clicar_imagem('temperatura_ambiente.png')
+clicar_imagem('dispositivo.png')
+clicar_imagem('adicionar.png')
+clicar_imagem('gerar.png')
+time.sleep(10)
+clicar_imagem('ferramentas.png')
+clicar_imagem('relatoriotexto.png')
+clicar_imagem('anticamaratexto.png')
+time.sleep(10)
 
-
-
-
-
-
-
+# Selecionar e copiar o texto gerado
+pyautogui.hotkey('ctrl', 'a')
+pyautogui.hotkey('ctrl', 'c')
